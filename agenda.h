1864133e20cad30c500f4698a5c97e52d908808a
martinriedweg <Igufmd3z@>
2017-10-12 09:51:55 +0200

    added structure

--- agenda.h
+++ agenda.h
@@ -3,4 +3,25 @@
 
 #endif //AGENDA_AGENDA_H
 
+int lenghtTitleAppointment = 0;
+
 int displayMenu();
+
+struct Date {
+    int day;
+    int month;
+    int year;
+};
+
+struct Schedule {
+    int hour;
+    int minute;
+};
+
+struct Appointment {
+    int date;
+    int startSchedule;
+    int endSchedule;
+    char title[lenghtTitleAppointment];
+    int remind = 0;
+};
